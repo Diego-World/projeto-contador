32f220e31f189ecd13bb6aa284778de55c25d071
Diego Gouveia <83849414+Diego-World@users.noreply.github.com>
2022-12-01 22:16:09 -0300

    first commit

---
+++ src/Contador.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o 1° parametro");
+        int parametroUm= sc.nextInt();
+        System.out.println("Digite o 2° parametro");
+        int parametroDois= sc.nextInt();
+
+
+        try{
+
+        contar(parametroUm,parametroDois);
+
+        }catch (ParametrosInvalidosException exception){
+            System.out.println("O Segundo parametro deve ser maior que o primeiro!");
+//            exception.printStackTrace();
+        }
+
+    }
+
+    static void contar( int parametroUm,int parametroDois ) throws ParametrosInvalidosException{
+        if(parametroUm>parametroDois){
+            throw new ParametrosInvalidosException();
+        }
+        int contagem = (parametroDois-parametroUm);
+        for(int i=1;i<=contagem;i++){
+
+            System.out.println("Imprimindo o número "+i);
+        }
+        System.out.println("Houveram "+contagem+" Iterações");
+    }
+
+}
---
+++ src/ParametrosInvalidosException.java
@@ -0,0 +1,9 @@
+public class ParametrosInvalidosException extends Exception{
+
+    public static void main(String[] args) {
+    }
+
+    public void ParametrosInvalidosException(int parametroUm,int parametroDois) {
+        if (parametroDois > parametroUm);
+    }
+}
